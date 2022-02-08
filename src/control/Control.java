d3a32c4af9c9365a1fbddea3146ad11bacad1ae5
jeremiecaron1 <jeremiecaron1@gmail.com>
2022-02-08 12:32:09 -0500

    a

--- src/control/Control.java
+++ src/control/Control.java
@@ -7,17 +7,15 @@ public class Control {
 
     // permet d'initialiser le frame Aquarium
     public static void main(String[] args) {
-        
+
         GUIIntro guiIntro = new GUIIntro();
-       
-    
-  
+
         guiIntro.setSize(300, 250);
         guiIntro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         guiIntro.setResizable(false);
         guiIntro.setLocationRelativeTo(null);
         guiIntro.setVisible(true);
-        
+
     }
 
 }
