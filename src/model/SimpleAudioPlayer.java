a700f17e407005b97a5f25110f53384c92b6d770
PherdTheBird <fredericchoiniere1@gmail.com>
2022-04-14 12:45:44 -0400

    pause poissons

--- src/model/SimpleAudioPlayer.java
+++ src/model/SimpleAudioPlayer.java
@@ -1,75 +1,63 @@
 package model;
 
-
 import java.io.File;
 import java.io.IOException;
-  
+
 import javax.sound.sampled.*;
 
-  
-public class SimpleAudioPlayer 
-{
-  
+public class SimpleAudioPlayer {
 
     Long currentFrame;
     Clip clip;
-      
+
     // statut du clip
     String status;
-      
+
     AudioInputStream audioInputStream;
     static String filePath = "res/sons/ambiance.wav";
-  
+
     // constructor to initialize streams and clip
     public SimpleAudioPlayer()
-        throws UnsupportedAudioFileException,
-        IOException, LineUnavailableException 
-    {
+            throws UnsupportedAudioFileException,
+            IOException, LineUnavailableException {
         // create AudioInputStream object
-        audioInputStream = 
-                AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
-          
+        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+
         // create clip reference
         clip = AudioSystem.getClip();
-          
+
         // open audioInputStream to the clip
         clip.open(audioInputStream);
-        
+
         // pour qu'il n'arrete pas à la fin de son cycle
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
-      
+
     // Method pour jouer la musique
-    public void play() 
-    {
-        //start the clip
+    public void play() {
+        // start the clip
         clip.start();
-          
+
         status = "play";
     }
-      
+
     // Method pour pauser la music
-    public void pause() 
-    {
-        if (status.equals("paused")) 
-        {
+    public void pause() {
+        if (status.equals("paused")) {
             System.out.println("audio is already paused");
             return;
         }
-        this.currentFrame = 
-        this.clip.getMicrosecondPosition();
+        this.currentFrame = this.clip.getMicrosecondPosition();
         clip.stop();
         status = "paused";
     }
-      
+
     // Method pour reprendre après une pause
     public void resumeAudio() throws UnsupportedAudioFileException,
-                                IOException, LineUnavailableException 
-    {
-        if (status.equals("play")) 
-        {
-            System.out.println("Audio is already "+
-            "being played");
+            IOException, LineUnavailableException {
+        if (status.equals("play")) {
+            System.out.println("Audio is already " +
+                    "being played");
             return;
         }
         clip.close();
@@ -77,16 +65,14 @@ public class SimpleAudioPlayer
         clip.setMicrosecondPosition(currentFrame);
         this.play();
     }
-      
 
     // Method to reset audio stream
     public void resetAudioStream() throws UnsupportedAudioFileException, IOException,
-                                            LineUnavailableException 
-    {
+            LineUnavailableException {
         audioInputStream = AudioSystem.getAudioInputStream(
-        new File(filePath).getAbsoluteFile());
+                new File(filePath).getAbsoluteFile());
         clip.open(audioInputStream);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
-  
+
 }
