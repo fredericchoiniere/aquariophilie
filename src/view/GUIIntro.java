8ff18f67397faa50d81bfd77ffbd2c5a932a11b8
ThunderLiight <justin.plouffe@live.ca>
2022-05-03 11:46:47 -0400

    panelshop

--- src/view/GUIIntro.java
+++ src/view/GUIIntro.java
@@ -5,19 +5,7 @@
 package view;
 
 import javax.swing.*;
-
-import model.jeu.Argent;
-import model.plantes.BlueBlue;
-import model.plantes.Erdtree;
-import model.plantes.JavaFern;
-import model.plantes.ScarletRot;
-import model.poissons.PoissonBetta;
-import model.poissons.PoissonNeo;
-import model.poissons.PoissonRouge;
-import model.poissons.PoissonTetra;
-
 import java.awt.event.*;
-
 import java.awt.*;
 
 public class GUIIntro extends JFrame implements ActionListener {
@@ -34,109 +22,49 @@ public class GUIIntro extends JFrame implements ActionListener {
         // Entrer dans l'application pour tester
         JPanel panelIntro = new JPanel();
         panelIntro.setOpaque(false);
+        panelIntro.setLayout(null);
 
-        // Gestionnaire
-        GridBagLayout g = new GridBagLayout();
-        panelIntro.setLayout(g);
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.weightx = 0.5;
-        constraints.weighty = 0.5;
-        constraints.gridwidth = 3;
-        constraints.gridheight = 1;
-        constraints.fill = 0;
+        JLayeredPane layerIntro = new JLayeredPane();
 
-        //JLayeredPane layerIntro = new JLayeredPane();
-        //layerIntro.setBounds(0, 0, 500, 40);
-        //layerIntro.setOpaque(false);
-
-        //Animation d'arriere-plan
-        JLabel lblGifIntro = new JLabel(new ImageIcon("res/background/intro.gif"));
-        //lblGifIntro.setBounds(0, 0, 500, 380);
-        //getContentPane().add(lblGifIntro);
-        //setLocationRelativeTo(null);
-        lblGifIntro.setVisible(true);
-        g.addLayoutComponent(lblGifIntro, constraints);
-        lblGifIntro.add(Box.createGlue(), constraints);
-        //layerIntro.add(lblGifIntro, 0);
-        
-
-
+        JLabel lblGif = new JLabel();
+        lblGif.setIcon(new ImageIcon("res/background/intro2.png"));
+        lblGif.setBounds(0,0,500,400);
+        panelIntro.add(lblGif);
+        //layerIntro.add(lblGif,0);
+            
         // label pour le titre
         titre = new JLabel("Aquariophilie");
         titre.setFont(new Font("Verdana", Font.BOLD, 30));
-        //layerIntro.add(titre, 1);
-        titre.setVisible(true);
-        panelIntro.add(titre, constraints);
+        titre.setBounds(100, 0, 350, 100);
+        panelIntro.add(titre);
+        //layerIntro.add(titre,1);
         
-
         // création des boutons pour les nombreuses action possible sur le Frame intro
         // -----------------------------------------------------------------------------------------------------------------------------
 
         // nouvelle partie
         btnNouvellePartie = new JButton("Nouvelle Partie");
         btnNouvellePartie.addActionListener(this);
-        //btnNouvellePartie.setBounds(50,50, 200, 20);
-        //layerIntro.add(btnNouvellePartie, 1);
-        btnNouvellePartie.setVisible(true);
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.gridwidth = 1;
-        constraints.gridheight = 1;
-        constraints.fill = 2;
-        panelIntro.add(btnNouvellePartie, constraints);
-
-        // charger partie
-        btnChargerPartie = new JButton("Charger Partie");
-        btnChargerPartie.addActionListener(this);
-        constraints.gridx = 1;
-        constraints.gridy = 2;
-        panelIntro.add(btnChargerPartie, constraints);
+        btnNouvellePartie.setBounds(150, 0, 200, 50);
+        //btnNouvellePartie.setVisible(true);
+        panelIntro.add(btnNouvellePartie);
+        //layerIntro.add(btnNouvellePartie);
+       
+        
 
         // mode evaluation
         btnModeEvaluation = new JButton("Mode Evaluation");
         btnModeEvaluation.addActionListener(this);
-        constraints.gridx = 1;
-        constraints.gridy = 3;
-        panelIntro.add(btnModeEvaluation, constraints);
         
         // credit
         btnCredit = new JButton("Credit");
         btnCredit.addActionListener(this);
-        //layerIntro.add(btnCredit, 1);
-        btnCredit.setVisible(true);
-        
-        constraints.gridx = 1;
-        constraints.gridy = 4;
-        panelIntro.add(btnCredit, constraints);
 
         // fermer l'application
         btClose = new JButton("Fermer");
         btClose.addActionListener(this);
-        //layerIntro.add(btClose, 1);
-        btClose.setVisible(true);
-        constraints.gridx = 0;
-        constraints.gridy = 5;
-        constraints.fill = 0;
-        panelIntro.add(btClose, constraints);
 
-        /*
-        // classement
-        btClassement = new JButton("Classement");
-        btClassement.addActionListener(this);
-        constraints.gridx = 1;
-        constraints.gridy = 5;
-        simplePanel.add(btClassement, constraints);
-
-        // inspirations
-        btInspiration = new JButton("Inspiration");
-        btInspiration.addActionListener(this);
-        constraints.gridx = 2;
-        constraints.gridy = 5;
-        simplePanel.add(btInspiration, constraints);
-        */
-        //panelIntro.add(layerIntro);
+        panelIntro.add(layerIntro);
         add(panelIntro);
 
         // action listener sur la touche enter pour aller plus vite
@@ -163,8 +91,6 @@ public class GUIIntro extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) { // initialise GuiMain lorsque l'on clique sur enter
         // bouton pour une nouvelle partie
         if (e.getSource() == btnNouvellePartie) {
-            // SimpleAudioPlayer.playMusic();
-
             GUIMain aquarium = new GUIMain();
             aquarium.setResizable(false);
             aquarium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -172,93 +98,16 @@ public class GUIIntro extends JFrame implements ActionListener {
             aquarium.setLocationRelativeTo(null);
             aquarium.setVisible(true);
             setVisible(false);
-
-            PoissonRouge.prix = 50;
-            PoissonBetta.prix = 500;
-            PoissonTetra.prix = 200;
-            PoissonNeo.prix = 125;
-            JavaFern.prix = 450;
-            BlueBlue.prix = 200;
-            ScarletRot.prix = 1000;
-            Erdtree.prix = 5000;
-
-        }
-
-        // bouton pour reprendre la partie
-        if (e.getSource() == btnChargerPartie) {
-            // TODO
         }
 
         // bouton pour le mode évaluation
         if (e.getSource() == btnModeEvaluation) {
-            GUIMain aquarium = new GUIMain();
-            aquarium.setResizable(false);
-            aquarium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            aquarium.pack();
-            aquarium.setLocationRelativeTo(null);
-            aquarium.setVisible(true);
-            setVisible(false);
-
-            // enlevement du systeme de progression
-            PoissonRouge.prix = 0;
-            PoissonBetta.prix = 0;
-            PoissonTetra.prix = 0;
-            PoissonNeo.prix = 0;
-            JavaFern.prix = 0;
-            BlueBlue.prix = 0;
-            ScarletRot.prix = 0;
-            Erdtree.prix = 0;
-            Argent.argent = 0;
-            Argent.montant = "∞";
-            GUIMain.label_argent_aqua.setText("∞");
-            GUIMain.label_argent_shop.setText("∞");
-            Argent.normal = false;
-
+            // TODO
         }
 
         // bouton pour les credits
         if (e.getSource() == btnCredit) { // TODO: à revoir
-            /*
-             * JFrame frame = new JFrame("Credit");
-             * 
-             * frame.setSize(500, 400);
-             * frame.setResizable(false);
-             * frame.setLocationRelativeTo(null);
-             * frame.setVisible(true);
-             * JPanel panel = new JPanel();
-             * 
-             * GridBagLayout g = new GridBagLayout(); // Gestionnaire
-             * panel.setLayout(g);
-             * GridBagConstraints constraints = new GridBagConstraints();
-             * 
-             * JLabel label = new JLabel("Créateur:");
-             * constraints.gridx = 0;
-             * constraints.gridy = 0;
-             * constraints.weightx = 0.5;
-             * constraints.weighty = 0.5;
-             * constraints.gridwidth = 1;
-             * constraints.gridheight = 1;
-             * constraints.fill = 0;
-             * panel.add(label, constraints);
-             * 
-             * JLabel createur1 = new JLabel("Jeremie Caron");
-             * constraints.gridx = 1;
-             * constraints.gridy = 0;
-             * panel.add(createur1, constraints);
-             * 
-             * JLabel createur2 = new JLabel("Frederic Choiniere");
-             * constraints.gridx = 1;
-             * constraints.gridy = 1;
-             * panel.add(createur2, constraints);
-             * 
-             * JLabel createur3 = new JLabel("Justin Plouffe");
-             * constraints.gridx = 1;
-             * constraints.gridy = 2;
-             * panel.add(createur3, constraints);
-             * 
-             * frame.add(panel);
-             * add(frame);
-             */
+           
         }
 
         // bouton pour fermer l'application
@@ -266,19 +115,7 @@ public class GUIIntro extends JFrame implements ActionListener {
             System.exit(0);
         }
 
-        // bouton pour les classements
-        if (e.getSource() == btClassement) {
-            // TODO
-        }
-
-        // bouton pour les inspirations du projet
-        if (e.getSource() == btInspiration) {
-            // TODO
-        }
-        
     }
-
-    
 
     // --------------------------------------------------------------------------------------------------------------------------------
 
