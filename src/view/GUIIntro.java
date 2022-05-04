05d6e16822ce83d3a2381af9c04acf7abc2f6d55
jeremiecaron1 <jeremiecaron1@gmail.com>
2022-05-04 19:33:10 -0400

    xczc

--- src/view/GUIIntro.java
+++ src/view/GUIIntro.java
@@ -1,122 +1,92 @@
-//Jérémie Caron     itération 1
-//Classe pour l'affichage du frame d'introduction
-// Pour l'itération 3
-
 package view;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.*;
 
-public class GUIIntro extends JFrame implements ActionListener {
-
-    // créer les attributs de la classe
-    public JButton btnNouvellePartie, btnChargerPartie, btnModeEvaluation, btnCredit, btClose, btClassement,
+public class GUIIntro extends JPanel {
+    private Image image;
+    public static JLabel label;
+    public static JButton btnNouvellePartie, btnChargerPartie, btnModeEvaluation, btnCredit, btClose, btClassement,
             btInspiration;
-    public JLabel titre;
 
-    public GUIIntro() { // création du constructeur GUIIntro
+    static ImageIcon img = new ImageIcon("res/background/icone_aquariophilie.png");
 
-        setTitle("Aquariophilie");
+    GUIIntro(Image image) {
+        this.image = image;
+    }
 
-        // Entrer dans l'application pour tester
-        JPanel panelIntro = new JPanel();
-        panelIntro.setOpaque(false);
-        panelIntro.setLayout(null);
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+    }
 
-        JLayeredPane layerIntro = new JLayeredPane();
+    public static void guiIntroFrame() {
+        try {
+            JFrame frame = new JFrame("Image");
 
-        JLabel lblGif = new JLabel();
-        lblGif.setIcon(new ImageIcon("res/background/intro2.png"));
-        lblGif.setBounds(0,0,500,400);
-        //panelIntro.add(lblGif);
-        layerIntro.add(lblGif,0);
-            
-        // label pour le titre
-        titre = new JLabel("Aquariophilie");
-        titre.setFont(new Font("Verdana", Font.BOLD, 30));
-        titre.setBounds(100, 0, 350, 100);
-        panelIntro.add(titre);
-        //layerIntro.add(titre,1);
-        
-        // création des boutons pour les nombreuses action possible sur le Frame intro
-        // -----------------------------------------------------------------------------------------------------------------------------
+            Image image = Toolkit.getDefaultToolkit().getImage(
+                    "res/background/intro.gif");
 
-        // nouvelle partie
-        btnNouvellePartie = new JButton("Nouvelle Partie");
-        btnNouvellePartie.addActionListener(this);
-        btnNouvellePartie.setBounds(150, 0, 200, 50);
-        btnNouvellePartie.setVisible(true);
-        //panelIntro.add(btnNouvellePartie);
-        layerIntro.add(btnNouvellePartie,1);
-       
-        
+            GUIIntro imagePanel = new GUIIntro(image);
+            imagePanel.setLayout(null);
 
-        // mode evaluation
-        btnModeEvaluation = new JButton("Mode Evaluation");
-        btnModeEvaluation.addActionListener(this);
-        
-        // credit
-        btnCredit = new JButton("Credit");
-        btnCredit.addActionListener(this);
-
-        // fermer l'application
-        btClose = new JButton("Fermer");
-        btClose.addActionListener(this);
-
-        panelIntro.add(layerIntro);
-        add(panelIntro);
-
-        // action listener sur la touche enter pour aller plus vite
-        btnNouvellePartie.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+            btnNouvellePartie = new JButton("Nouvelle Partie");
+            btnNouvellePartie.addActionListener((ActionListener) new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
                     GUIMain aquarium = new GUIMain();
                     aquarium.setResizable(false);
                     aquarium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                     aquarium.pack();
                     aquarium.setLocationRelativeTo(null);
                     aquarium.setVisible(true);
-                    setVisible(false);
+                    frame.setVisible(false);
                 }
-            }
-        });
+            });
+            btnNouvellePartie.addKeyListener(new java.awt.event.KeyAdapter() {
+                public void keyPressed(java.awt.event.KeyEvent evt) {
+                    if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+                        GUIMain aquarium = new GUIMain();
+                        aquarium.setResizable(false);
+                        aquarium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        aquarium.pack();
+                        aquarium.setLocationRelativeTo(null);
+                        aquarium.setVisible(true);
+                        frame.setVisible(false);
+                    }
+                }
+            });
+            btnNouvellePartie.setBounds(150, 0, 200, 50);
+            btnNouvellePartie.setVisible(true);
+            imagePanel.add(btnNouvellePartie);
 
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLocationRelativeTo(null);
+            frame.add(imagePanel);
+            frame.setSize(500, 400);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.setIconImage(img.getImage());
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            frame.setTitle("Aquariophilie");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * ActionListeners pour la classe GUIIntro
+    /*
+     * public static void main(String[] args) {
+     * 
+     * SwingUtilities.invokeLater(new Runnable() {
+     * public void run() {
+     * createAndShowUI();
+     * }
+     * });
+     * }
      */
-    @Override
-    public void actionPerformed(ActionEvent e) { // initialise GuiMain lorsque l'on clique sur enter
-        // bouton pour une nouvelle partie
-        if (e.getSource() == btnNouvellePartie) {
-            GUIMain aquarium = new GUIMain();
-            aquarium.setResizable(false);
-            aquarium.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            aquarium.pack();
-            aquarium.setLocationRelativeTo(null);
-            aquarium.setVisible(true);
-            setVisible(false);
-        }
-
-        // bouton pour le mode évaluation
-        if (e.getSource() == btnModeEvaluation) {
-            // TODO
-        }
-
-        // bouton pour les credits
-        if (e.getSource() == btnCredit) { // TODO: à revoir
-           
-        }
-
-        // bouton pour fermer l'application
-        if (e.getSource() == btClose) {
-            System.exit(0);
-        }
-
-    }
-
-    // --------------------------------------------------------------------------------------------------------------------------------
-
 }
